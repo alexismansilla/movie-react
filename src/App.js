1e8c55b14669cfdcee0c4562f03898afefb32371
Alexis Mansilla Ruiz <alexis.mansilla04@gmail.com>
2021-08-03 19:24:03 -0400

    Add pages.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import logo from './logo.svg';
+import { Layout } from 'antd';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+//pages
+import Home from './pages/home';
+import NewMovies from './pages/new-movies';
+import Popular from './pages/popular';
+import Search from './pages/search';
+import Movie from './pages/movie';
+import Error404 from './pages/error404';
+
+export default function App() {
+  const { Header, Content } = Layout;
+
   return (
-    <div className="App">
-      <h1>Estamos en App</h1>
-    </div>
+    <Layout>
+      <Router>
+        <Header>
+          <p>Header</p>
+        </Header>
+
+        <Content>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies />
+            </Route>
+            <Route path="/popular" exact={true}>
+              <Popular />
+            </Route>
+            <Route path="/search" exact={true}>
+              <Search />
+            </Route>
+            <Route path="/movie/:id" exact={true}>
+              <Movie />
+            </Route>
+            <Route path="*">
+              <Error404 />
+            </Route>
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
   );
 }
-
-export default App;
