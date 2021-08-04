658764aa5764d920f4bad903c0c8e57a36357333
Alexis Mansilla Ruiz <alexis.mansilla04@gmail.com>
2021-08-03 20:32:09 -0400

    Fix indentation.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom';
 import './Menu.scss';
 
 export default function Menu() {
-	return (
-		<div className="menu">
-			<div className="menu-logo">
-				<Navbar theme="dark" mode="horizontal" defaultSelectedKey={["1"]}>
-					<Navbar.Item key="1">
-						<Link to="/">Home</Link>
-					</Navbar.Item>
-				</Navbar>
-			</div>
-		</div>
-	)
+  return (
+    <div className="menu">
+      <div className="menu-logo">
+        <Navbar theme="dark" mode="horizontal" defaultSelectedKey={["1"]}>
+          <Navbar.Item key="1">
+          <Link to="/">Home</Link>
+          </Navbar.Item>
+        </Navbar>
+      </div>
+    </div>
+  )
 }
