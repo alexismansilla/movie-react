729001647621f4377c51fdd8e3e65d9d0fc45886
Alexis Mansilla Ruiz <alexis.mansilla04@gmail.com>
2021-08-05 16:24:11 -0400

    Add movies to the slider.

--- src/components/SliderMovies/SliderMovies.js
+++ src/components/SliderMovies/SliderMovies.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { Carousel, Button } from 'antd';
+import { Link } from 'react-router-dom';
 
-import './SliderMovies';
+import './SliderMovies.scss';
 
-export default function SliderMovies(props) {
-  const { movies } = props;
-  const { result, loading } = movies;
-
-  if(loading || result ) {
-    return "Loading...";
+export default function SliderMovies({movies: { result, loading } = {}}) {
+  if (loading || !result) {
+    return "Loading...!"
   }
+
+  const { results } = result || {};
 
   return (
     <Carousel autoplay className="slider-movies">
-      <p>Waiting...</p>
+      { results.map(movie => (
+        <Movie key={movie.id} movie={movie} />
+      ))}
     </Carousel>
   );
+}
+
+function Movie({ movie }) {
+  const { id, title, overview, backdrop_path } = movie || {};
+  const backdropPath = `https://image.tmdb.org/t/p/original/${backdrop_path}`
+
+  return (
+    <div className="slider-movies__movie" style={{ backgroundImage: `url('${backdropPath}')`}}>
+      <div className="slider-movies__movie-info">
+        <div>
+          <h2>{title}</h2>
+          <p>{overview}</p>
+          <Link to={`/movie/${id}`}>
+            <Button type="primary">More Information</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
