a3127920ae3f6c6bf2acefc15bb0edb701652442
Alexis Mansilla Ruiz <alexis.mansilla04@gmail.com>
2021-08-17 15:35:23 -0400

    Update Pagination.js

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -4,15 +4,15 @@ import Pagination from 'rc-pagination';
 import './Pagination.scss';
 
 export default function PaginationMovies(props) {
-	const { currentPage, totalItems, onChangePage } = props;
+  const { currentPage, totalItems, onChangePage } = props;
 
-	return (
-		<Pagination
-			className='pagination'
-			current={currentPage}
-			total={totalItems}
-			pageSize={20}
-			onChange={onChangePage}
-		/>
-	)
+  return (
+    <Pagination
+      className='pagination'
+      current={currentPage}
+      total={totalItems}
+      pageSize={20}
+      onChange={onChangePage}
+    />
+  )
 }
